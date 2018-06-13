ec8c65ebf6894a0aa068d0a4be3816cb53541f11
Krzysztof Kin <sirex0@gmail.com>
2018-06-13 22:18:52 +0200

    code comment

--- app/src/main/java/cloud/krzysztofkin/newsapp/DataUtils.java
+++ app/src/main/java/cloud/krzysztofkin/newsapp/DataUtils.java
@@ -122,7 +122,7 @@ public class DataUtils {
                 Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
             }
         } catch (IOException e) {
-            Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+            Log.e(LOG_TAG, "Problem retrieving the JSON results.", e);
         } finally {
             if (urlConnection != null) {
                 urlConnection.disconnect();
